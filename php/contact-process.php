31e813ea27376fcd824fb60e43f3efa86a0f78a8
Lucas Blancas <lablancas@gmail.com>
2017-09-19 01:10:04 -0500

    use phpmailer installed via composer

--- php/contact-process.php
+++ php/contact-process.php
@@ -1,10 +1,46 @@
 <?php
-require_once 'Mail.php';
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-$host = "ssl://email-smtp.us-west-2.amazonaws.com";
-$port = "465";
-$username = 'AKIAJFO7ACH5R7GRUHQA';
-$password = 'At+hixEplzjygepMYCGQEhuXw8K7c1ikDatX60TFfAxb';
+// Modify the path in the require statement below to refer to the
+// location of your Composer autoload.php file.
+require_once '../vendor/autoload.php';
+
+// Instantiate a new PHPMailer
+$mail = new PHPMailer(true);
+
+// Tell PHPMailer to use SMTP
+$mail->isSMTP();
+
+// Replace sender@example.com with your "From" address.
+// This address must be verified with Amazon SES.
+$mail->setFrom('sender@example.com', 'Sender Name');
+
+// Replace recipient@example.com with a "To" address. If your account
+// is still in the sandbox, this address must be verified.
+// Also note that you can include several addAddress() lines to send
+// email to multiple recipients.
+$mail->addAddress('lablancas@gmail.com', 'Lucas Blancas');
+
+// Replace smtp_username with your Amazon SES SMTP user name.
+$mail->Username = 'AKIAJFO7ACH5R7GRUHQA';
+
+// Replace smtp_password with your Amazon SES SMTP password.
+$mail->Password = 'At+hixEplzjygepMYCGQEhuXw8K7c1ikDatX60TFfAxb';
+
+// Specify a configuration set. If you do not want to use a configuration
+// set, comment or remove the next line.
+// $mail->addCustomHeader('X-SES-CONFIGURATION-SET', 'ConfigSet');
+
+// If you're using Amazon SES in a region other than US West (Oregon),
+// replace email-smtp.us-west-2.amazonaws.com with the Amazon SES SMTP
+// endpoint in the appropriate region.
+$mail->Host = 'email-smtp.us-west-2.amazonaws.com';
+
+// The port you will connect to on the Amazon SES SMTP endpoint.
+$mail->Port = 465;
 
 // Configure your Subject Prefix and Recipient here
 $subjectPrefix = 'GeoIntegrity - Website - Contact Form';
@@ -43,30 +79,29 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
             <strong>Message: </strong>'.nl2br($message).'<br />
         ';
 
-        $headers = array();
-        $headers['MIME-Version'] = '1.1';
-        $headers['Content-type'] = 'text/html; charset=utf-8';
-        $headers['Content-Transfer-Encoding'] = '8bit';
-        $headers['Date'] = date('r', $_SERVER['REQUEST_TIME']);
-        $headers['Message-ID'] = '<' . $_SERVER['REQUEST_TIME'] . md5($_SERVER['REQUEST_TIME']) . '@' . $_SERVER['SERVER_NAME'] . '>';
-        $headers['From'] = $name . ' <' . $email . '> ';
-        $headers['Return-Path'] = $emailTo;
-        $headers['Reply-To'] = $email;
-        $headers['X-Mailer'] = 'PHP/'. phpversion();
-        $headers['X-Originating-IP'] = $_SERVER['SERVER_ADDR'];
+        // The subject line of the email
+        $mail->Subject = $subject;
 
-        $smtp = Mail::factory('smtp',
-          array ('host' => $host,
-            'port' => $port,
-            'auth' => true,
-            'username' => $username,
-            'password' => $password));
+        // The HTML-formatted body of the email
+        $mail->Body = $body;
 
-        $mail = $smtp->send($emailTo, $headers, $body);
+        // Tells PHPMailer to use SMTP authentication
+        $mail->SMTPAuth = true;
 
-        if (PEAR::isError($mail)) {
+        // Enable SSL encryption
+        $mail->SMTPSecure = 'ssl';
+
+        // Tells PHPMailer to send HTML-formatted email
+        $mail->isHTML(true);
+
+        // The alternative email body; this is only displayed when a recipient
+        // opens the email in a non-HTML email client. The \r\n represents a
+        // line break.
+        $mail->AltBody = "Name: $name\r\nEmail: $email\r\nMessage: $message";
+
+        if(!$mail->send()) {
           $data['success'] = false;
-          $data['errors']  = $mail->getMessage();
+          $data['errors']  = $mail->ErrorInfo;
         } else {
           $data['success'] = true;
           $data['confirmation'] = 'Congratulations. Your message has been sent successfully';
